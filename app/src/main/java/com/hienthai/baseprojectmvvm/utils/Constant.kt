e916a3e87e20297b66a3e6dc14b223b71ec91992
hien.thai <hien.thai@ntq-solution.com.vn>
2024-01-31 14:59:53 +0700

    # remove call

--- app/src/main/java/com/hienthai/baseprojectmvvm/utils/Constant.kt
+++ app/src/main/java/com/hienthai/baseprojectmvvm/utils/Constant.kt
@@ -1,5 +1,8 @@
 package com.hienthai.baseprojectmvvm.utils
 
 object Constant {
-    const val BASE_URL = "https://newsapi.org"
+    const val BASE_URL = "http://10.0.18.66:3000/"
+    const val TITLE = "TITLE"
+    const val NAME = "NAME"
+    const val BODY = "BODY"
 }
