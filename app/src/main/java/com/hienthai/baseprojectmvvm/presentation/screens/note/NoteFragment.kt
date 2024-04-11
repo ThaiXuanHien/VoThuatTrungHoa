47c269f947e00d2dc716ef7e3f9ee4a06bdbc757
hien.thai <hien.thai@ntq-solution.com.vn>
2024-04-11 09:59:07 +0700

    # remove datetime

--- app/src/main/java/com/hienthai/baseprojectmvvm/presentation/screens/note/NoteFragment.kt
+++ app/src/main/java/com/hienthai/baseprojectmvvm/presentation/screens/note/NoteFragment.kt
@@ -2,6 +2,7 @@ package com.hienthai.baseprojectmvvm.presentation.screens.note
 
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -27,11 +28,9 @@ class NoteFragment : BaseFragment<FragmentNoteBinding>() {
     private val viewModel: NoteViewModel by viewModel()
     private val adapter by lazy { NoteAdapter(::onItemClicked) }
     private var selectedNote: NoteEntity? = null
-    private lateinit var singleBuilder: SingleDateAndTimePickerDialog.Builder
 
     override fun initView() {
         super.initView()
-        initDateTime()
         binding.run {
             rcvNotes.layoutManager = LinearLayoutManager(context)
             SwipeHelper(viewModel::deleteNote).attachToRecyclerView(rcvNotes)
@@ -42,8 +41,6 @@ class NoteFragment : BaseFragment<FragmentNoteBinding>() {
 
             tvDate.setSafeClickListener {
 //                navigator?.navigate(screen = noteDetailScreen())
-
-                singleBuilder.display()
             }
         }
     }
@@ -102,35 +99,6 @@ class NoteFragment : BaseFragment<FragmentNoteBinding>() {
         selectedNote = noteEntity
         binding.edtInputNote.setText(noteEntity.title)
         binding.edtInputNote.setSelection(binding.edtInputNote.text.length)
-    }
-
-    private fun initDateTime() {
-        val calendar = Calendar.getInstance()
-        val defaultDate = calendar.time
-
-        singleBuilder = SingleDateAndTimePickerDialog.Builder(context)
-            .setTimeZone(TimeZone.getDefault())
-            .bottomSheet()
-            .curved()
-            .minutesStep(5)
-            .displayYears(true)
-            .todayText("今日")
-            .displayListener(object : SingleDateAndTimePickerDialog.DisplayListener {
-                override fun onDisplayed(picker: SingleDateAndTimePicker) {
-                    // Do something when displayed
-                }
-
-                override fun onClosed(picker: SingleDateAndTimePicker) {
-                    // Do something when closed
-                }
-            })
-            .listener { date ->
-                val cal = Calendar.getInstance()
-                cal.time = date
-                val year = cal.get(Calendar.YEAR)
-                val month = cal.get(Calendar.MONTH)
-                val day = cal.get(Calendar.DAY_OF_MONTH)
-            }
     }
 
 }
